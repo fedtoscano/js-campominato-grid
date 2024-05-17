509a7446042cf4e82c16defcfd6e499de3a53af7
fedtoscano <federicotoscano611@gmail.com>
2024-05-17 14:22:56 +0200

    script: aggiunto il ciclo for per stampare gli elementi

--- script/script.js
+++ script/script.js
@@ -2,6 +2,12 @@ const gridEl = document.getElementById("grid")
 
 for (let index = 0; index < 100; index++) {
     const newSquare = document.createElement("article");
-    newSquare
-    
+    newSquare.classList.add("square")
+
+    const textContent = document.createElement("span")
+    textContent.append(index+1)
+
+    newSquare.appendChild(textContent)
+    gridEl.appendChild(newSquare)
+
 }
